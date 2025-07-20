37403bf0b8672b2b3b6341b7f0feea9c38306c15
Young <young20050727@gmail.com>
2025-07-20 08:47:57 +0800

    add flake.nix and .gitignore

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "NixOS configuration for server deployment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        packages.default = pkgs.hello;
+      }
+    ) // {
+      nixosConfigurations.server01 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/server01/configuration.nix
+        ];
+      };
+    };
+}
