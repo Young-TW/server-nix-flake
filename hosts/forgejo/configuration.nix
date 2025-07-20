3960fff5febb4ea0517d8d953f894f7d1a8bd5ba
Young <young20050727@gmail.com>
2025-07-20 16:51:21 +0800

    feat: update forgejo configuration.nix

--- hosts/forgejo/configuration.nix
+++ hosts/forgejo/configuration.nix
@@ -1,37 +1,133 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-  ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Bootloader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/sda"; # 請依你 VM 磁碟實際裝置修改（如 /dev/sda）
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "forgejo";
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
   time.timeZone = "Asia/Taipei";
 
+  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-  console.keyMap = "us";
 
-  users.users.nixos = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ];
-    shell = pkgs.bash;
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "zh_TW.UTF-8";
+    LC_IDENTIFICATION = "zh_TW.UTF-8";
+    LC_MEASUREMENT = "zh_TW.UTF-8";
+    LC_MONETARY = "zh_TW.UTF-8";
+    LC_NAME = "zh_TW.UTF-8";
+    LC_NUMERIC = "zh_TW.UTF-8";
+    LC_PAPER = "zh_TW.UTF-8";
+    LC_TELEPHONE = "zh_TW.UTF-8";
+    LC_TIME = "zh_TW.UTF-8";
   };
 
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.nixos = {
+    isNormalUser = true;
+    description = "nixos";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "nixos";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim
-    git
-    curl
-    htop
-    tmux
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+     git
+     vim
+     wget
   ];
 
-  services.openssh.enable = true;
-  services.openssh.permitRootLogin = "no";
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-  system.stateVersion = "24.05";
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+  services.openssh.enable = true;
+
+  services.forgejo = {
+    enable = true;
+    user = "git";
+    group = "git";
+    settings = {
+      server = {
+        ROOT_URL = "http://localhost:3000/";
+        DOMAIN = "localhost";
+        HTTP_PORT = 3000;
+        DISABLE_SSH = true;
+      };
+      ssh ={
+        START_SSH_SERVER = true;
+        SSH_PORT = 2222;
+      };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 22 2222 3000 ];
+
+  users.groups.git = {};
+
+  users.users.git = {
+    isSystemUser = true;
+    home = "/var/lib/forgejo";
+    group = "git";
+  };
 }
