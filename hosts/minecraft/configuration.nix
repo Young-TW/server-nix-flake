b4e306454b9f4821192337e47e788f685522acf7
Young <young20050727@gmail.com>
2025-08-17 14:37:34 +0800

    fix: set minecraft flake to pure module

--- hosts/minecraft/configuration.nix
+++ hosts/minecraft/configuration.nix
@@ -1,65 +1,33 @@
+{ config, pkgs, lib, ... }:
 {
-  description = "Minecraft Server Flake";
+  # 若這台有對應的硬體檔，記得引入
+  # imports = [ ./hardware-configuration.nix ];
 
-  nixConfig = {
-    substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-    ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+  users.users.minecraft-server = {
+    isSystemUser = true;
+    group = "minecraft-server";
+    home = "/var/lib/minecraft-server";
   };
+  users.groups.minecraft-server = {};
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  networking.firewall.allowedTCPPorts = [ 25565 ];
 
-  outputs = { self, nixpkgs, ... }@inputs: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    nixosConfigurations.minecraft = nixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = { inherit inputs; };
-      modules = [
-
-        ({ config, pkgs, ... }: {
-          users.users.minecraft-server = {
-            isSystemUser = true;
-            group = "minecraft-server";
-            home = "/var/lib/minecraft-server";
-          };
-          users.groups.minecraft-server = {};
-
-          networking.firewall.allowedTCPPorts = [ 25565 ];
-
-          systemd.services.minecraft-server = {
-            description = "Minecraft Server";
-            after = [ "network-online.target" ];
-            wants = [ "network-online.target" ];
-            wantedBy = [ "multi-user.target" ];
-
-            # if pkgs.papermc not exists, build will fail and need to be fixed.
-            serviceConfig = {
-              ExecStart = "${pkgs.papermc}/bin/minecraft-server -Xmx2G -Xms1G";
-              WorkingDirectory = "/var/lib/minecraft-server";
-              StateDirectory = "minecraft-server";
-              User = "minecraft-server";
-              Group = "minecraft-server";
-              Restart = "always";
-              RestartSec = "5s";
-            };
-          };
-        })
-      ];
-    };
-
-    devShells.${system}.default = let
-      mkShell = pkgs.mkShell.override { stdenv = pkgs.stdenvNoCC; };
-    in mkShell {
-      packages = with pkgs; [ nil ];
+  systemd.services.minecraft-server = {
+    description = "Minecraft Server";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      # 依實際可執行檔名調整（可能是 papermc / paper 等）
+      ExecStart = "${pkgs.papermc}/bin/minecraft-server -Xmx2G -Xms1G";
+      WorkingDirectory = "/var/lib/minecraft-server";
+      StateDirectory = "minecraft-server";
+      User = "minecraft-server";
+      Group = "minecraft-server";
+      Restart = "always";
+      RestartSec = "5s";
     };
   };
+
+  system.stateVersion = "24.05"; # 依你的系統版本調整
 }
