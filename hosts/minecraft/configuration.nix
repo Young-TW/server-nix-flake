e676aeb9b61a9d8c92fbff35c3219077d83decda
Young <young20050727@gmail.com>
2025-08-17 14:24:25 +0800

    fix: update minecraft server config

--- hosts/minecraft/configuration.nix
+++ hosts/minecraft/configuration.nix
@@ -3,12 +3,9 @@
 
   nixConfig = {
     substituters = [
-      "https://mirrors.ustc.edu.cn/nix-channels/store"
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
     ];
-
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
@@ -20,26 +17,49 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: let
-    pkgs = import nixpkgs { system = "x86_64-linux"; };
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
   in {
-    nixosConfigurations.clyz-minecraft = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+    nixosConfigurations.minecraft = nixpkgs.lib.nixosSystem {
+      inherit system;
       specialArgs = { inherit inputs; };
-
       modules = [
-        ./system
+
+        ({ config, pkgs, ... }: {
+          users.users.minecraft-server = {
+            isSystemUser = true;
+            group = "minecraft-server";
+            home = "/var/lib/minecraft-server";
+          };
+          users.groups.minecraft-server = {};
+
+          networking.firewall.allowedTCPPorts = [ 25565 ];
+
+          systemd.services.minecraft-server = {
+            description = "Minecraft Server";
+            after = [ "network-online.target" ];
+            wants = [ "network-online.target" ];
+            wantedBy = [ "multi-user.target" ];
+
+            # if pkgs.papermc not exists, build will fail and need to be fixed.
+            serviceConfig = {
+              ExecStart = "${pkgs.papermc}/bin/minecraft-server -Xmx2G -Xms1G";
+              WorkingDirectory = "/var/lib/minecraft-server";
+              StateDirectory = "minecraft-server";
+              User = "minecraft-server";
+              Group = "minecraft-server";
+              Restart = "always";
+              RestartSec = "5s";
+            };
+          };
+        })
       ];
     };
 
-    devShells.x86_64-linux.default = let
+    devShells.${system}.default = let
       mkShell = pkgs.mkShell.override { stdenv = pkgs.stdenvNoCC; };
-    in
-      mkShell {
-        packages = with pkgs; [
-          nil
-        ];
-      };
+    in mkShell {
+      packages = with pkgs; [ nil ];
+    };
   };
-
-  jvmOpts = "-Xmx2048M -Xms2048M -Djava.net.preferIPv4Stack=true";
 }
