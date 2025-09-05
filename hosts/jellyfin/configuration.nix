c306d46c678a051c3006de3b6c6c7ac5807014da
Young <young20050727@gmail.com>
2025-09-05 08:21:13 +0800

    fix: add parameter to jellyfin config

--- hosts/jellyfin/configuration.nix
+++ hosts/jellyfin/configuration.nix
@@ -1,4 +1,9 @@
-{
+{ config, pkgs, ... }: {
+  imports = [ ../common.nix ];
+
+  networking.firewall.allowedTCPPorts = [ 8096 8920 ];
+  networking.firewall.allowedUDPPorts = [ 1900 ];
+
   services.jellyfin.enable = true;
   environment.systemPackages = [
     pkgs.jellyfin
