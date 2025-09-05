716feb85fe44638ec9b49d13039de14aee96a1b5
Young <young20050727@gmail.com>
2025-09-05 08:28:54 +0800

    fix: jellyfin config build failed

--- hosts/jellyfin/configuration.nix
+++ hosts/jellyfin/configuration.nix
@@ -1,13 +1,14 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }: {
   imports = [ ../common.nix ];
 
   networking.firewall.allowedTCPPorts = [ 8096 8920 ];
   networking.firewall.allowedUDPPorts = [ 1900 ];
 
   services.jellyfin.enable = true;
-  environment.systemPackages = [
-    pkgs.jellyfin
-    pkgs.jellyfin-web
-    pkgs.jellyfin-ffmpeg
+
+  environment.systemPackages = with pkgs; [
+    jellyfin
+    jellyfin-web
+    jellyfin-ffmpeg
   ];
 }
